e980c529ba192a78f89d1b69064a94816f4b9e88
mayi5147 <92247557+mayi5147@users.noreply.github.com>
2022-07-13 19:11:43 +0800

    Update MSG1500.sh

--- public/MSG1500.sh
+++ public/MSG1500.sh
@@ -20,7 +20,7 @@ sed -i 's/SYS_USER_ROOT		"admin"/SYS_USER_ROOT		"'$user_name'"/g' $default_file
 sed -i 's/DEF_ROOT_PASSWORD	"admin"/DEF_ROOT_PASSWORD	"'$user_password'"/g' $default_file
 
 #echo "修改 LAN IP 地址"
-sed -i "s/192.168.2.1/$lan_ip/g" $default_path/defaults.h
+sed -i "s/192.168.2/$lan_ip/g" $default_path/defaults.h
 
 #echo "修改 LAN DHCP 地址"
 sed -i "s/192.168.2.2/$dhcp_beg/g" $default_path/defaults.h
@@ -28,6 +28,3 @@ sed -i "s/192.168.2.254/$dhcp_end/g" $default_path/defaults.h
 
 #echo "更新版本号时间"
 sed -i "s/FIRMWARE_BUILDS_REV=.*/FIRMWARE_BUILDS_REV=$version_time/g" ./versions.inc
-
-# 其他
-echo "CONFIG_FIRMWARE_INCLUDE_ZEROTIER=y" >>.config   # zerotier ~1.3M
